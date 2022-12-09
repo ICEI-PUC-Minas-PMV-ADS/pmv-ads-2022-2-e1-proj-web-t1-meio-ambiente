f4471d9f1103a58c362f9ad215bf2b3654bb58ca
Pricilla Baier <114036574+PricillaAraujo@users.noreply.github.com>
2022-12-09 18:21:07 -0300

    Delete home.script.js

--- home.script.js
+++
@@ -1,9 +0,0 @@
-function video(){
-    location.href = "vea.html";
-}
-function noticias(){
-    location.href = "noticias.html";
-}
-function localizacao(){
-    location.href = "localizacao.html";
-}
